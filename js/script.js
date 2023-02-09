fccb6d420e01a6a90889c54251a80079bd9b2dc8
Daiki Iwata <direction2423@gmail.com>
2023-02-08 17:27:43 -0800

    Create the array of quotes

--- js/script.js
+++ js/script.js
@@ -3,14 +3,21 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
-
+const quotes = [
+  {quote: "Watch your thoughts, for they will become actions. Watch your actions, for they will become habits. Watch your habits for they will forge your character. Watch your character, for it will make your destiny.", source: "Margaret Hilda Thatcher"},
+  {quote: "I Have A Dream", source: "Martin Luther King Junior"},
+  {quote: "Stay hungry, stay foolish", source: "Steve Jobs"},
+  {quote: "Do not worry, You will know my name soon.", source: "Daiki Iwata", year: "2023"},
+  {quote: "Try not to become a man of success, but rather try to become a man of value.", source: "Albert Einstein"},
+  {quote: "The difference between winning and losing is most often not quitting", source: "Walt Disney", citation: "https://www.positivitysparkles.com/quote/difference-winning-losing-oftennot-quitting-walt-disney/"}
+];
 
 
 /***
