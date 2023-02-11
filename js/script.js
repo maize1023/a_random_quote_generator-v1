7cd39f631e6aae1773698036ff7f5bcefd31ccb4
Daiki Iwata <direction2423@gmail.com>
2023-02-10 22:31:04 -0800

    add new property

--- js/script.js
+++ js/script.js
@@ -7,12 +7,12 @@ project 1 - A Random Quote Generator
 
 // ①Store many objects that must contain quote and source in the quotes array
 const quotes = [
-  {quote: "Watch your thoughts, for they will become actions. Watch your actions, for they will become habits. Watch your habits for they will forge your character. Watch your character, for it will make your destiny.", source: "Margaret Hilda Thatcher"},
-  {quote: "I Have A Dream", source: "Martin Luther King Junior"},
-  {quote: "Stay hungry, stay foolish", source: "Steve Jobs"},
-  {quote: "Spread love everywhere you go. Let no one ever come to you without leaving happier.", source: "Mother Teresa", citation: "blog.hubspot.com", year: "2023"},
-  {quote: "Try not to become a man of success, but rather try to become a man of value.", source: "Albert Einstein"},
-  {quote: "The difference between winning and losing is most often not quitting", source: "Walt Disney"}
+  {quote: "Watch your thoughts, for they will become actions. Watch your actions, for they will become habits. Watch your habits for they will forge your character. Watch your character, for it will make your destiny.", source: "Margaret Hilda Thatcher", tags: "enlightenment"},
+  {quote: "I Have A Dream", source: "Martin Luther King Junior", tags: "history"},
+  {quote: "Stay hungry, stay foolish", source: "Steve Jobs", tags: "enlightenment"},
+  {quote: "Spread love everywhere you go. Let no one ever come to you without leaving happier.", source: "Mother Teresa", citation: "blog.hubspot.com", year: "2023", tags: "enlightenment"},
+  {quote: "Try not to become a man of success, but rather try to become a man of value.", source: "Albert Einstein", tags: "enlightenment"},
+  {quote: "The difference between winning and losing is most often not quitting", source: "Walt Disney", tags: "enlightenment"}
 ];
 
 
